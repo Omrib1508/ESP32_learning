31d9a9a41a12f995c5f2bf88dba2bf1c2dfb7d0b
Omrib1508 <omribensh15@gmail.com>
2023-07-14 15:41:33 +0300

    First Commit: Blinking Led with dutycycle change

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,25 @@
 #include <Arduino.h>
 
-const uint8_t PotenResistor = 4; // led at GPIO D4
-int potenValue = 0, precision = 2;
-float voltage, ratio, res1, res2;
-
-void printElcetircValues(int analogVal) {
-    char res1Buffer[15], res2Buffer[15];
-    char voltageBuffer[10], printBuffer[100];
-    
-    ratio = float(analogVal)/float(0xFFF);
-    voltage = 3.3 * ratio;
-    res1 = 10000 * (1 - ratio);
-    res2 = 10000 - res1;
-    dtostrf(voltage, precision + 3, precision, voltageBuffer);
-    dtostrf(res1, precision + 6, precision, res1Buffer);
-    dtostrf(res2, precision + 6, precision, res2Buffer);
-    //sprintf(printBuffer, "Vin=3.30[V], Vout=%s[V]", voltageBuffer);
-    sprintf(printBuffer, "Vin=3.30[V], Vout=%s[V], R1=%s[Ohm], R2=%s[Ohm]", voltageBuffer, res1Buffer, res2Buffer);
-    Serial.println(printBuffer);
-}
+const uint8_t RedLed = 4; // led at GPIO D4
+const int     freq = 5000;
+const int     ledChannel = 0;
+const uint8_t resultion = 8;
 
 void setup() {
-  Serial.begin(115200);
-
-  pinMode(PotenResistor, INPUT);
+  ledcSetup(ledChannel, freq, resultion);
+  ledcAttachPin(RedLed, ledChannel);
   delay(2000);
 }
 
 void loop() {
-  potenValue = analogRead(PotenResistor);
-  printElcetircValues(potenValue);
-  delay(500);
+  for (int dutyCycle = 0; dutyCycle <= 0xFF; dutyCycle++)
+  {
+    ledcWrite(ledChannel, dutyCycle);
+    delay(15);
+  }
+  for (int dutyCycle = 0xFF; dutyCycle >= 0; dutyCycle--)
+  {
+    ledcWrite(ledChannel, dutyCycle);
+    delay(15);
+  }
 }
