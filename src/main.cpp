50154ae673cb4f6eb4aa6c821549e1c5c0aa978e
Omrib1508 <omribensh15@gmail.com>
2023-07-15 18:22:10 +0300

    Commit: channging the GPIO of led and buzzing time

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
 #include <Arduino.h>
 
-#define SECONDS 10
+#define SECONDS 5
 
-const uint8_t RedLed = 34; // led at GPIO D34
+const uint8_t RedLed = 25; // led at GPIO D34
 const uint8_t SIRmotion = 27; // SIR motion sensor at GPIO D27
 const uint8_t Buzzer = 26; // Buzzer at GPIO D26
-int           LedState = LOW;
-int           buzzerState = LOW;
 
 unsigned long interuptTime = 0;
-unsigned long currentTime = 0;
+unsigned long currentTime = millis();
 
 bool          isTimerStart = false;
 bool          isMotionDitact = false;
@@ -24,11 +22,12 @@ void IRAM_ATTR detectsMovement() {
 void setup() {
   Serial.begin(115200);
 
+  pinMode(SIRmotion, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(SIRmotion), detectsMovement, RISING);
+
   pinMode(RedLed, OUTPUT);
   pinMode(Buzzer, OUTPUT);
 
-  pinMode(SIRmotion, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(SIRmotion), detectsMovement, RISING);
 
   digitalWrite(RedLed, LOW);
   digitalWrite(Buzzer, LOW);
