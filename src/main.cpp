aac70242f8d35e08b64816e6d76021a66f512366
Omrib1508 <omribensh15@gmail.com>
2023-07-25 10:50:23 +0300

    Commit: change in main and WiFiNetwork

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,51 @@
 #include <WiFi.h>
 #include "WiFiNetwork.h"
 
-const uint8_t RedLed = 26; // led at GPIO D26
-const uint8_t GreenLed = 27; // led at GPIO D27
+const uint8_t RedLed        = 13; // led at GPIO D13
+const uint8_t GreenLed      = 12; // led at GPIO D12
+const uint8_t BlueLed       = 14; // led at GPIO D14
 String        readBuffer;
-String        RedLedState = "off";   // status of the red led
-String        GreenLedState = "off"; // status of the green led
-WiFiServer    server(80); // Set web server port number to 80
-unsigned long currentTime = millis();
+String        RedLedState   = "0";   // status of the red led
+String        GreenLedState = "0"; // status of the green led
+String        BlueLedState  = "0";   // status of the red led
 char*         ssid;
 char*         password;
+unsigned long currentTime = millis();
 int           menuChoose;
+int           pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+const int     freq          = 5000;
+const int     redChannel    = 0;
+const int     greenChannel  = 1;
+const int     blueChannel   = 2;
+const int     resolution    = 8;
+
 void setup() {
   wl_status_t connecntionStatus;
   int          scanIteration = 0;
-  pinMode(RedLed, OUTPUT);
-  pinMode(GreenLed, OUTPUT);
-
-  digitalWrite(RedLed, LOW);
-  digitalWrite(GreenLed, LOW);
   
+  ledcSetup(redChannel, freq, resolution);
+  ledcSetup(greenChannel, freq, resolution);
+  ledcSetup(blueChannel, freq, resolution);
+  
+  ledcAttachPin(RedLed, redChannel);
+  ledcAttachPin(greenChannel, GreenLed);
+  ledcAttachPin(blueChannel, BlueLed);
+
   Serial.begin(115200);
   Serial.println("Choose from menu:");
   Serial.println("1 - Scan networks");
   Serial.println("2 - Create Station throw network");
   while (!Serial.available()) {}
   menuChoose = Serial.readStringUntil('\n').toInt();
+}
 
-  switch (menuChoose)
+void loop() {
+  
+ 
+}
+
+
+switch (menuChoose)
   {
     case 1:
       Serial.println("Scanning for network. Please enter the number of scans:");
@@ -74,103 +92,3 @@ void setup() {
       }
       break;
   }
-}
-
-void loop() {
-  WiFiClient    client;
-  String        clientInputBuffer;
-  String        header; // variable for storing HTTP request
-  char          readByte;
-  unsigned long initClientTime = 0;
-
-  client = server.available();   // Listen for incoming clients
-  
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client");          // print a message out in the serial port
-    clientInputBuffer = "";                // make a String to hold incoming data from the client
-    currentTime = millis();
-    initClientTime = currentTime;
-    while (client.connected() && (currentTime - initClientTime <= CLIENTCONNECTIONTIMEOUT)) {  // loop while the client's connected
-      currentTime = millis();
-      if (client.available()) {             // if there's bytes to read from the client,
-        readByte = client.read();             // read a byte, then
-        Serial.write(readByte);                    // print it out the serial monitor
-        header += readByte;
-        if (readByte == '\n') {                    // if the byte is a newline character
-          if (clientInputBuffer.length() == 0) {
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /26/on") >= 0) {
-              Serial.println("GPIO 26 on");
-              RedLedState = "on";
-              digitalWrite(RedLed, HIGH);
-            } else if (header.indexOf("GET /26/off") >= 0) {
-              Serial.println("GPIO 26 off");
-              RedLedState = "off";
-              digitalWrite(RedLed, LOW);
-            } else if (header.indexOf("GET /27/on") >= 0) {
-              Serial.println("GPIO 27 on");
-              GreenLedState = "on";
-              digitalWrite(GreenLed, HIGH);
-            } else if (header.indexOf("GET /27/off") >= 0) {
-              Serial.println("GPIO 27 off");
-              GreenLedState = "off";
-              digitalWrite(GreenLed, LOW);
-            }
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #555555;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP32 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 26  
-            client.println("<p>Red Led - State " + RedLedState + "</p>");
-            // If the output26State is off, it displays the ON button       
-            if (RedLedState=="off") {
-              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 27  
-            client.println("<p>Green Led - State " + GreenLedState + "</p>");
-            // If the output27State is off, it displays the ON button       
-            if (GreenLedState=="off") {
-              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            clientInputBuffer = "";
-          }
-        } else if (readByte != '\r') {  // if you got anything else but a carriage return character,
-          clientInputBuffer += readByte;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
-}
